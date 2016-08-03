fc81d865ab74827dab540d4c8eeaaf18125cf7d6
Matias Insaurralde <matias@insaurral.de>
2016-08-03 16:41:49 -0400

    Doing some json stuff in Python

--- python/dispatcher.py
+++ python/dispatcher.py
@@ -1,2 +1,7 @@
-def process(object):
-    print("processing object", object)
+import json
+
+def process(serialized_object):
+    object = json.loads(serialized_object)
+    object['Message'] = "modified message"
+    serialized_object = json.dumps(object)
+    return serialized_object
