35c0d1c9b7cd1cc3ad23b93ae807edc172224246
Matias Insaurralde <matias@insaurral.de>
2016-08-03 16:41:23 -0400

    Modifying Python_Dispatch return type

--- python/binding.h
+++ python/binding.h
@@ -6,7 +6,7 @@
 static int Python_Init();
 static int Python_LoadDispatcher();
 static void Python_SetEnv(char*);
-static void Python_Dispatch(char*);
+static char* Python_Dispatch(char*);
 
 static char* dispatcher_module_name = "dispatcher";
 static char* hook_name = "process";
