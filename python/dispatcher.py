a119ada7a4d6648ee635494ce0346f2ecdd3f3bf
Matias Insaurralde <matias@insaurral.de>
2016-08-04 16:54:35 -0400

    Implementing the hook

--- python/dispatcher.py
+++ python/dispatcher.py
@@ -1,4 +1,4 @@
-import json, ujson
+import json, ujson, msgpack
 
 def dispatch_json_string(serialized_object):
     object = json.loads(serialized_object)
@@ -12,6 +12,17 @@ def dispatch_ujson_string(serialized_object):
     serialized_object = ujson.dumps(object)
     return serialized_object
 
-def dispatch_native_object( name, message, timestamp ):
+def dispatch_native_object(name, message, timestamp):
     message = "modified message"
     return name, message, str(timestamp)
+
+def dispatch_msgpack_object(serialized_object):
+    # print("serialized_object:", len(serialized_object))
+    # print(serialized_object)
+    object = msgpack.unpackb(serialized_object, use_list=False)
+    # object[b'message'] = b'modified message'
+    # object[b'ts'] = 100
+    new_object = msgpack.packb(object)
+    # print("new_object:", len(new_object))
+    # print(new_object)
+    return new_object
