f2c7817362b13a69bb48a943c2f41656948de6ee
Matias Insaurralde <matias@insaurral.de>
2016-08-04 15:51:04 -0400

    Sample file for MsgPackMode

---
+++ examples/test_msgpack.go
@@ -0,0 +1,42 @@
+package main
+
+import(
+  "github.com/matiasinsaurralde/dispatcher-benchmark"
+
+  "time"
+  "fmt"
+)
+
+func main() {
+
+  fmt.Println("main")
+
+  var err error
+
+  dispatcher.PythonSetPath("/Users/matias/dev/dispatcher/python")
+
+  err = dispatcher.PythonInit()
+  err = dispatcher.PythonLoadDispatcher()
+
+  if err != nil {
+    panic(err)
+  }
+
+  shinyDispatcher := dispatcher.NewDispatcher(dispatcher.MsgPackMode)
+
+  object := dispatcher.Object{
+    Name: "theObject",
+    Message: "the message",
+    Timestamp: time.Now().Unix(),
+  }
+
+  fmt.Println( shinyDispatcher.Dispatch(&object) )
+  /*
+  var output interface{}
+  output, err = shinyDispatcher.Dispatch(&object)
+  var outputString string
+  outputString = output.(string)
+
+  fmt.Println("output = ", outputString)
+  */
+}
