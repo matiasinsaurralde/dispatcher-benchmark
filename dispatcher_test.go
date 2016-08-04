1ff8f628304225475939b59f7ee2017cea1b6c7d
Matias Insaurralde <matias@insaurral.de>
2016-08-03 23:29:22 -0400

    Extend benchmarks

--- dispatcher_test.go
+++ dispatcher_test.go
@@ -4,7 +4,11 @@ import(
   "testing"
   "time"
   "fmt"
+
+  "encoding/json"
 )
+
+const iterations int = 15000
 
 func init() {
   fmt.Println("Benchmark setup")
@@ -15,21 +19,60 @@ func init() {
   PythonLoadDispatcher()
 }
 
-func BenchmarkJsonMode(b *testing.B) {
-  d := NewDispatcher(JsonMode)
+func BenchmarkJsonStringMode(b *testing.B) {
+  d := NewDispatcher(JsonStringMode)
 
-  for i := 0; i < 200; i++ {
+  for i := 0; i < iterations; i++ {
     object := Object{
       Name: "theObject",
       Message: "the message",
       Timestamp: time.Now().Unix(),
     }
-    time.Sleep(1*time.Millisecond)
-    d.Dispatch(&object)
+    // time.Sleep(1*time.Millisecond)
+    var output interface{}
+    output, _ = d.Dispatch(&object)
+    outputString := output.(string)
+
+    var deserializedObject Object
+    json.Unmarshal( []byte(outputString), &deserializedObject)
   }
 
 }
 
+func BenchmarkUjsonStringMode(b *testing.B) {
+  d := NewDispatcher(UJsonStringMode)
+
+  for i := 0; i < iterations; i++ {
+    object := Object{
+      Name: "theObject",
+      Message: "the message",
+      Timestamp: time.Now().Unix(),
+    }
+    // time.Sleep(1*time.Millisecond)
+    var output interface{}
+    output, _ = d.Dispatch(&object)
+    outputString := output.(string)
+
+    var deserializedObject Object
+    json.Unmarshal( []byte(outputString), &deserializedObject)
+  }
+
+}
+
+func BenchmarkNativeMode(b *testing.B) {
+  d := NewDispatcher(NativeMode)
+
+  for i := 0; i < iterations; i++ {
+    object := Object{
+      Name: "theObject",
+      Message: "the message",
+      Timestamp: time.Now().Unix(),
+    }
+    // time.Sleep(1*time.Millisecond)
+    d.Dispatch(&object)
+  }
+
+}
 /*
 func BenchmarkMsgpackMode(b *testing.B) {
   d := NewDispatcher(MsgPackMode)
