edccf4cc60a2888f18cf0a689669247c030df623
Matias Insaurralde <matias@insaurral.de>
2016-08-03 23:29:38 -0400

    Updating binding

--- python/binding.go
+++ python/binding.go
@@ -10,6 +10,8 @@ package python
 
 #include "binding.h"
 
+#include "../dispatcher.h"
+
 static int Python_Init() {
   Py_Initialize();
   printf("Py_Initialize\n");
@@ -37,6 +39,10 @@ static int Python_LoadDispatcher() {
 
   dispatcher_dispatch = PyDict_GetItemString(dispatcher_dict, hook_name);
 
+  dispatch_json_string_hook = PyDict_GetItemString(dispatcher_dict, dispatch_json_string);
+  dispatch_ujson_string_hook = PyDict_GetItemString(dispatcher_dict, dispatch_ujson_string);
+  dispatch_native_object_hook = PyDict_GetItemString(dispatcher_dict, dispatch_native_object);
+
   if( dispatcher_dispatch == NULL ) {
     PyErr_Print();
     return -1;
@@ -54,9 +60,7 @@ static void Python_SetEnv(char* python_path) {
   setenv("PYTHONPATH", python_path, 1 );
 }
 
-static char* Python_Dispatch(char* object) {
-  printf("dispatching: %s\n", object);
-
+static char* Python_DispatchJsonString(char* object) {
   PyObject *args = PyTuple_Pack( 1, PyUnicode_FromString(object) );
   PyObject *result = PyObject_CallObject( dispatcher_dispatch, args );
   if( result == NULL ) {
@@ -67,12 +71,63 @@ static char* Python_Dispatch(char* object) {
     return payload;
   }
 }
+
+static char* Python_DispatchUJsonString(char* object) {
+  PyObject *args = PyTuple_Pack( 1, PyUnicode_FromString(object) );
+  PyObject *result = PyObject_CallObject( dispatch_ujson_string_hook, args );
+  if( result == NULL ) {
+    PyErr_Print();
+    return NULL;
+  } else {
+    char *payload = PyUnicode_AsUTF8(result);
+    return payload;
+  }
+}
+
+static NativeObject* Python_DispatchNativeObject(void* p) {
+  NativeObject* obj = (NativeObject*)p;
+  PyObject *n = Py_BuildValue("i", obj->timestamp);
+  PyObject *args = PyTuple_Pack( 3, PyUnicode_FromString(obj->name), PyUnicode_FromString(obj->message), n );
+  PyObject *result = PyObject_CallObject( dispatch_native_object_hook, args );
+  if( result == NULL ) {
+    PyErr_Print();
+    return NULL;
+  } else {
+    PyObject* tupleName = PyTuple_GetItem(result, 0);
+    PyObject* tupleMessage = PyTuple_GetItem(result, 1);
+    PyObject* tupleTimestamp = PyTuple_GetItem(result, 2);
+
+    char* name = PyUnicode_AsUTF8(tupleName);
+    char* message = PyUnicode_AsUTF8(tupleMessage);
+    char* timestamp_str = PyUnicode_AsUTF8(tupleTimestamp);
+
+    int timestamp = atoi(timestamp_str);
+
+    int len = sizeof(NativeObject) + strlen(name) + strlen(message) + sizeof(int) + 1;
+    NativeObject* obj = (NativeObject*) malloc( len );
+
+    obj->name = name;
+    obj->message = message;
+    obj->timestamp = timestamp;
+
+    return obj;
+  }
+  return NULL;
+};
 */
 import "C"
 
 import(
   "errors"
+  // "fmt"
+  "unsafe"
 )
+
+type Object struct {
+  Name string `msg:"name"`
+  Message string `msg:"message"`
+  Timestamp int64 `msg:"ts"`
+}
 
 func Init() error {
   var err error
@@ -97,12 +152,42 @@ func SetPath(path string) {
   C.Python_SetEnv((CPath))
 }
 
-func DispatchString(object []byte) string {
+func DispatchJsonString(object []byte) string {
   s := string(object)
   CObject := C.CString(s)
   var output *C.char
-  output = C.Python_Dispatch(CObject)
+  output = C.Python_DispatchJsonString(CObject)
   return C.GoString(output)
+}
+
+func DispatchUJsonString(object []byte) string {
+  s := string(object)
+  CObject := C.CString(s)
+
+  var output *C.char
+  output = C.Python_DispatchUJsonString(CObject)
+
+  C.free(unsafe.Pointer(CObject))
+
+  var outputStr string
+  outputStr = C.GoString(output)
+
+  return outputStr
+}
+
+func DispatchNativeObject(object unsafe.Pointer) Object {
+  var Cobj *C.struct_NativeObject
+  Cobj = C.Python_DispatchNativeObject(object)
+
+  obj := Object{
+    Name: C.GoString(Cobj.name),
+    Message: C.GoString(Cobj.message),
+    Timestamp: int64(Cobj.timestamp),
+  }
+
+  C.free(unsafe.Pointer(Cobj))
+
+  return obj
 }
 
 func DispatchBytes(object []byte) {
--- python/binding.h
+++ python/binding.h
@@ -6,10 +6,21 @@
 static int Python_Init();
 static int Python_LoadDispatcher();
 static void Python_SetEnv(char*);
-static char* Python_Dispatch(char*);
+
+static char* Python_DispatchJsonString(char*);
+static char* Python_DispatchUJsonString(char*);
 
 static char* dispatcher_module_name = "dispatcher";
 static char* hook_name = "process";
+
+static char* dispatch_json_string = "dispatch_json_string";
+static PyObject* dispatch_json_string_hook;
+
+static char* dispatch_ujson_string = "dispatch_ujson_string";
+static PyObject* dispatch_ujson_string_hook;
+
+static char* dispatch_native_object = "dispatch_native_object";
+static PyObject* dispatch_native_object_hook;
 
 static PyObject* dispatcher;
 static PyObject* dispatcher_module;
